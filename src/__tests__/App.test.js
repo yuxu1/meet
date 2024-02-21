b17a2e78752e577cf3ab42da734d0ac95bfa4c5e
Yufan <yuxu1@users.noreply.github.com>
2024-02-20 22:29:28 -0800

    wrote tests and code for feature 1-scenario 1

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable testing-library/no-node-access */
+import { render } from '@testing-library/react';
+import App from '../App';
+
+describe('<App /> component', () => {
+  test('renders list of events', () => {
+    const AppDOM = render(<App />).container.firstChild;
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+  });
+
+});
